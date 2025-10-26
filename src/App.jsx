ea42419012a4f5117311ae7c0ca4ab2937603ec6
BilalAhmed9515 <bilal959515@gmail.com>
2025-10-26 16:59:22 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,27 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Navbar from './Components/Navbar/Navbar'
+import Home from './Components/Home/Home'
+import About_Us from './Components/About_Us/About_Us'
+import Services from './Components/Services/Services'
+import Contact_Us from './Components/Contact_Us/Contact_Us'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path='./' element={<Home/>}/>
+      <Route path='about_us' element={<About_Us/>}/>
+      <Route path='services' element={<Services/>}/>
+      <Route path='contact_us' element={<Contact_Us/>}/>
+    </Routes>
+    </BrowserRouter>
       
     </>
   )
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import { VscMenu } from "react-icons/vsc";
+import { RxCross1 } from "react-icons/rx";
+
+
 
 const Navbar = () => {
-  return (
-    <>Navbar</>
-  )
+
+    const [menuOpen, setMenuOpen] = useState(false)
+
+    const openmenu = () => {
+        setMenuOpen(!menuOpen)
+    }
+    return (
+        <>
+            <div className='flex justify-between shadow-lg h-16 sticky top-0 left-0 items-center lg:px-40 px-16'>
+                <div><NavLink to="">Logo</NavLink></div>
+                <div>
+                    <ul className='lg:flex gap-20 hidden '>
+                        <NavLink to="">Home</NavLink>
+                        <NavLink to="">About Us</NavLink>
+                        <NavLink to="">Services</NavLink>
+                        <NavLink to="">Contact Us</NavLink>
+                    </ul>
+                </div>
+
+                <div className='lg:hidden text-2xl cursor-pointer'>
+                    {
+                        menuOpen ? (
+                            <RxCross1 onClick={openmenu}/>
+                        ) : (
+                            <VscMenu onClick={openmenu} />
+                        )
+                    }
+                    
+                    
+                </div>
+
+                {/* mobile menu */}
+                {
+                    menuOpen && (
+                        <div className='lg:hidden absolute top-16 shadow-lg w-[100%] left-0 flex flex-col justify-center items-center p-4'>
+                            <ul className='flex flex-col gap-4'>
+                                <NavLink to="" onClick={openmenu}>Home</NavLink>
+                                <NavLink to="" onClick={openmenu}>About Us</NavLink>
+                                <NavLink to="" onClick={openmenu}>Services</NavLink>
+                                <NavLink to="" onClick={openmenu}>Contact Us</NavLink>
+                            </ul>
+                        </div>
+                    )
+                }
+
+            </div>
+        </>
+    )
 }
 
 export default Navbar
